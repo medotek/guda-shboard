d81e4a2b6a0e567b6092188abfbe07618e58c391
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2022-03-05 17:49:47 +0100

    Update for prod

---
+++ src/Command/UpdateHoyoPostListCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Command;
+
+use App\Controller\EncryptionManagerController;
+use App\Controller\HoyolabPostDiscordNotificationController;
+use App\Repository\HoyolabPostRepository;
+use App\Repository\HoyolabPostUserRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class UpdateHoyoPostListCommand extends Command
+{
+    protected static $defaultName = 'app:guda:hoyo-update-postlist';
+    private EntityManagerInterface $entityManager;
+    private HttpClientInterface $client;
+    private HoyolabPostUserRepository $hoyolabPostUserRepository;
+    private HoyolabPostRepository $hoyolabPostRepository;
+    private Security $security;
+    private UserRepository $userRepository;
+    private SerializerInterface $serializer;
+    private EncryptionManagerController $encryptionManager;
+
+    public function __construct(
+        string $name = null,
+        EntityManagerInterface $entityManager,
+        HttpClientInterface $client,
+        HoyolabPostUserRepository $hoyolabPostUserRepository,
+        HoyolabPostRepository $hoyolabPostRepository,
+        UserRepository              $userRepository,
+        SerializerInterface         $serializer,
+        Security                    $security,
+        EncryptionManagerController $encryptionManager
+    )
+    {
+        parent::__construct($name);
+
+        $this->entityManager = $entityManager;
+        $this->client = $client;
+        $this->hoyolabPostUserRepository = $hoyolabPostUserRepository;
+        $this->hoyolabPostRepository = $hoyolabPostRepository;
+        $this->userRepository = $userRepository;
+        $this->serializer = $serializer;
+        $this->security = $security;
+        $this->encryptionManager = $encryptionManager;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHelp('This command allows you to update and send a notification to all hoyolab users (a webhook needs to be configured)');
+    }
+
+    /**
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $taskForce = new \App\Controller\HoyolabPostsWebhookController(
+            $this->client,
+            $this->hoyolabPostRepository,
+            $this->security,
+            $this->entityManager,
+            $this->hoyolabPostUserRepository,
+            $this->userRepository,
+            $this->serializer,
+            $this->encryptionManager
+        );
+
+        $taskForce->updateHoyolabUserPostsList();
+        return Command::SUCCESS;
+    }
+}
